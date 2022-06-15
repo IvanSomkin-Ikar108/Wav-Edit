44ec77e384f756ac00d76269681b40c71cfbf983
Ikar108 <61326455+Ikar108@users.noreply.github.com>
2022-06-15 14:36:17 +0300

    Add command line parameters reading and modify .gitignore
    console: Add funtional to parse and work 'mode' and 'filepath' parameters.
    'testmode' and 'help' mods was added.
    
    .gitignore: remove module with disable  local IDE settings ignoring.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,55 @@
+#include <string>
 #include <iostream>
 
-int main() {
-  std::cout << "Hello, World!";
-  return 0;
+using std::cout;
+using std::endl;
+using std::string;
+
+struct params_test
+{
+  const string& fname;
+  params_test(const string& name): fname(name){};
+};
+
+void run_mode_test(params_test& cfg)
+{
+}
+
+void run_mode_help()
+{
+  cout << "USAGE:" << endl;
+  cout << "simple-dsp.exe MODE PARAM1 PARAM2 ..." << endl << endl;
+
+  cout << "MODE = help" << endl;
+  cout << "    Will print this help" << endl << endl;
+
+  cout << "MODE = testmode" << endl;
+  cout << "    PARAM1 = path to a file" << endl;
+  cout << "        Will test if the file is exists" << endl;
+	cout.flush();
+}
+
+int main(int argc, const char* argv[]) 
+{ 
+  if (argc > 2)
+  {
+    cout << "mode = '" << argv[1] << "'" << endl;
+    cout << "fname = '" << argv[2] << "'" << endl;
+
+    const string mode = string(argv[1]);
+    if ( mode == "help" || mode != "testmode" )
+    {
+      run_mode_help();
+    }
+    else
+    {
+      params_test cfg = params_test(string(argv[2]));
+      run_mode_test(cfg);
+    }
+  }
+  else
+  {
+    std::cout << "Hello, World!";
+  }
+    return 0;
 }
