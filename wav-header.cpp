d5f41c1d65fe82006ffd69d4e2712d3eba03268d
Ivan Somkin <somkin.van@yandex.ru>
2022-06-22 17:38:55 +0300

    refactor wav-header.h and add branching format read to constructor
    Add format namespace for audio formats. Move validation check to separate function from read function. Check for extension and fact subchunk during read.

--- wav-header.cpp
+++ wav-header.cpp
@@ -4,123 +4,150 @@
 
 using std::string;
 
-uint32_t _4x8_to_32_be(const std::vector<uint8_t>& byte_file, size_t idx)
+uint32_t _4x8_to_32_be(const std::vector<uint8_t> &byte_file, size_t idx)
 {
-    return byte_file[idx] << 24 | (byte_file[idx + 1] << 16) | (byte_file[idx + 2] << 8) | (byte_file[idx + 3]);
+  return byte_file[idx] << 24 | (byte_file[idx + 1] << 16) | (byte_file[idx + 2] << 8) | (byte_file[idx + 3]);
 }
 
-uint32_t _4x8_to_32_le(const std::vector<uint8_t>& byte_file, size_t idx)
+uint32_t _4x8_to_32_le(const std::vector<uint8_t> &byte_file, size_t idx)
 {
-    return byte_file[idx] | (byte_file[idx + 1] << 8) | (byte_file[idx + 2] << 16) | (byte_file[idx + 3] << 24);
+  return byte_file[idx] | (byte_file[idx + 1] << 8) | (byte_file[idx + 2] << 16) | (byte_file[idx + 3] << 24);
 }
 
-uint32_t _2x8_to_16_le(const std::vector<uint8_t>& byte_file, size_t idx)
+uint32_t _2x8_to_16_le(const std::vector<uint8_t> &byte_file, size_t idx)
 {
-    return byte_file[idx] | (byte_file[idx + 1] << 8);
+  return byte_file[idx] | (byte_file[idx + 1] << 8);
 }
 
-WavHeader::WavHeader (const std::vector<uint8_t>& byte_file)
+WavHeader::WavHeader(const std::vector<uint8_t> &byte_file)
 {
-    if (byte_file.size() >= 44)
-    {
-        uint32_t riff = _4x8_to_32_be(byte_file, 0);
-        uint32_t wave = _4x8_to_32_be(byte_file, 8);
-        if (riff == cn::RIFF && wave == cn::WAVE)
-        {
-            chunk_size = _4x8_to_32_le(byte_file, 4);
+  if (byte_file.size() < 80)
+  {
+    throw std::invalid_argument("Error: Bad file - File is too small!");
+  }
 
-            uint32_t fmt = _4x8_to_32_be(byte_file, 12);
-            subchunk1_size = _4x8_to_32_le(byte_file, 16);
-            // 16 is the standard PCM fmt chunksize 
-            if (fmt == cn::fmt && subchunk1_size == 16)
-            {
-                audio_format    = _2x8_to_16_le(byte_file, 20);
-                num_of_channels = _2x8_to_16_le(byte_file, 22);
-                samples_per_sec = _4x8_to_32_le(byte_file, 24);
-                bytes_per_sec   = _4x8_to_32_le(byte_file, 28);
-                block_align     = _2x8_to_16_le(byte_file, 32);
-                bits_per_sample = _2x8_to_16_le(byte_file, 34);
+  size_t offset = 0;
+  chunk_ID = _4x8_to_32_be(byte_file, 0);
+  chunk_size = _4x8_to_32_le(byte_file, 4);
+  format = _4x8_to_32_be(byte_file, 8);
 
-                int32_t data = _4x8_to_32_be(byte_file, 36);
-                subchunk2_size = _4x8_to_32_le(byte_file, 40);
-                //36 is the standard PCM difference between those two sizes + 0 or 1, if subchunk2_size is odd
-                if (data == cn::data && subchunk2_size + (36 + subchunk2_size % 2) == chunk_size)
-                {
-                    ok_flag = true;
-                }
-            }
-        }
-    }
-    else
-    {
-        throw std::invalid_argument("Error: Bad file: File is too small!");
-    }
+  subchunk1_ID = _4x8_to_32_be(byte_file, 12);
+  subchunk1_size = _4x8_to_32_le(byte_file, 16);
+  audio_format = _2x8_to_16_le(byte_file, 20);
+  num_of_channels = _2x8_to_16_le(byte_file, 22);
+  samples_per_sec = _4x8_to_32_le(byte_file, 24);
+  bytes_per_sec = _4x8_to_32_le(byte_file, 28);
+  block_align = _2x8_to_16_le(byte_file, 32);
+  bits_per_sample = _2x8_to_16_le(byte_file, 34);
+  extension_size = 0;
+
+  if (audio_format != format::WAVE_FORMAT_PCM)
+  {
+    extension_size = _2x8_to_16_le(byte_file, 36);
+  }
+
+  offset += extension_size;
+
+  fact_size = 0;
+  uint32_t next_subchunk_ID = _4x8_to_32_be(byte_file, 36 + offset);
+  if (next_subchunk_ID == id::fact)
+  {
+    fact_ID = next_subchunk_ID;
+    fact_size = _4x8_to_32_le(byte_file, 36 + offset);
+    sample_length = _4x8_to_32_le(byte_file, 40 + offset);
+    next_subchunk_ID = _4x8_to_32_be(byte_file, 36 + offset + 8 + fact_size);
+  }
+
+  offset += fact_size;
+  
+  subchunk2_ID = next_subchunk_ID;
+  subchunk2_size = _4x8_to_32_le(byte_file, 40 + offset);
 }
 
-WavHeader::WavHeader (const string& filepath): WavHeader(readfile(filepath, 44)) {}
+WavHeader::WavHeader(const string& filepath) : WavHeader(readfile(filepath, 80)) {}
 
 bool WavHeader::check_validity()
 {
-    return ok_flag;
+  if (chunk_ID != id::RIFF || format != id::WAVE || subchunk1_ID != id::fmt || subchunk2_ID != id::data)
+  {
+    return false;
+  }
+
+  if (audio_format == format::WAVE_FORMAT_PCM && subchunk1_size != 16)
+  {
+    return false;
+  }
+
+  if (audio_format == format::WAVE_FORMAT_EXTENSIBLE && subchunk1_size != 40)
+  {
+    return false;
+  }
+
+  if (20 + subchunk1_size + subchunk2_size + subchunk2_size % 2 != chunk_size)
+  {
+    return false;
+  }
+
+  return true;
 }
 
 uint16_t WavHeader::get_num_of_channels()
-{   
-    if (check_validity())
-    {
-        return num_of_channels;
-    }
-    throw std::invalid_argument("Error: Can't read invalid header!");
+{
+  if (check_validity())
+  {
+    return num_of_channels;
+  }
+  throw std::invalid_argument("Error: Can't read invalid header!");
 }
 
 uint32_t WavHeader::get_frequency()
 {
-    if (check_validity())
-    {
-        return samples_per_sec;
-    }
-    throw std::invalid_argument("Error: Can't read invalid header!");
+  if (check_validity())
+  {
+    return samples_per_sec;
+  }
+  throw std::invalid_argument("Error: Can't read invalid header!");
 }
 
 uint16_t WavHeader::get_depth_bit()
 {
-    if (check_validity())
-    {
-        return bits_per_sample;
-    }
-    throw std::invalid_argument("Error: Can't read invalid header!");
+  if (check_validity())
+  {
+    return bits_per_sample;
+  }
+  throw std::invalid_argument("Error: Can't read invalid header!");
 }
 
-string WavHeader::get_samples_type()
+std::string WavHeader::get_samples_type()
 {
-    if (check_validity())
-    {        
-        if (block_align == 2)
-        {
-            return "uint16_t";
-        }
-        if (block_align == 4)
-        {
-            return "uint32_t";
-        }
+  if (check_validity())
+  {
+    if (block_align == 2)
+    {
+      return "uint16_t";
     }
-    throw std::invalid_argument("Error: Can't read invalid header!");
+    if (block_align == 4)
+    {
+      return "uint32_t";
+    }
+  }
+  throw std::invalid_argument("Error: Can't read invalid header!");
 }
 
 uint32_t WavHeader::get_bit_per_second()
 {
-    if (check_validity())
-    {
-        return bytes_per_sec * 8;
-    }
-    throw std::invalid_argument("Error: Can't read invalid header!");
+  if (check_validity())
+  {
+    return bytes_per_sec * 8;
+  }
+  throw std::invalid_argument("Error: Can't read invalid header!");
 }
 
 double WavHeader::get_length()
 {
-     if (check_validity())
-    {
-        return double(subchunk2_size) / double(bytes_per_sec);
-    }
-    throw std::invalid_argument("Error: Can't read invalid header!");
+  if (check_validity())
+  {
+    return double(subchunk2_size) / double(bytes_per_sec);
+  }
+  throw std::invalid_argument("Error: Can't read invalid header!");
 }
--- wav-header.h
+++ wav-header.h
@@ -5,46 +5,58 @@
 #include <cstdint>
 #include <vector>
 
-namespace cn
+namespace id
 {
-    const uint32_t RIFF = 0x52494646;
-    const uint32_t WAVE = 0x57415645;
-    const uint32_t fmt  = 0x666D7420;
-    const uint32_t data = 0x64617461;
+  const uint32_t RIFF = 0x52494646;
+  const uint32_t WAVE = 0x57415645;
+  const uint32_t fmt  = 0x666D7420;
+  const uint32_t data = 0x64617461;
+  const uint32_t fact = 0x66616374;
+}
+
+namespace format
+{
+  const uint16_t WAVE_FORMAT_PCM        = 0x0001;
+	const uint16_t WAVE_FORMAT_ALAW       = 0x0006;
+  const uint16_t WAVE_FORMAT_MULAW      = 0x0007;	
+  const uint16_t WAVE_FORMAT_EXTENSIBLE = 0xFFFE;
 }
 
 class WavHeader
 {
-    private:
-    /*supply variables*/
-    bool ok_flag = false; 
-    /* The "RIFF" chunk descriptor */
-    uint32_t  chunk_ID = cn::RIFF;      // RIFF Header Magic header
-    uint32_t  chunk_size;               // RIFF Chunk Size
-    uint32_t  format = cn::WAVE;        // "WAVE" string
-    /* The "fmt" sub-chunk */
-    uint32_t  subchunk1_ID = cn::fmt;   // "fmt " string
-    uint32_t  subchunk1_size;           // Size of the fmt chunk
-    uint16_t  audio_format;             // Audio format 1=PCM, 6=mulaw, 7=alaw, 257=IBM Mu-Law, 258=IBM A-Law, 259=ADPCM
-    uint16_t  num_of_channels;          // Number of channels 1=Mono 2=Sterio
-    uint32_t  samples_per_sec;          // Sampling Frequency in Hz
-    uint32_t  bytes_per_sec;            // Bytes per second
-    uint16_t  block_align;              // 2=16-bit mono, 4=16-bit stereo
-    uint16_t  bits_per_sample;          // Number of bits per sample
-    /* The "data" sub-chunk */
-    uint32_t  subchunk2_ID = cn::data;  // "data" string
-    uint32_t  subchunk2_size;           // Sampled data length
-
-    public:
-    WavHeader (const std::vector<uint8_t>& byte_file);
-    WavHeader (const std::string& filepath);
-    bool check_validity();
-    uint16_t get_num_of_channels();
-    uint32_t get_frequency();
-    uint16_t get_depth_bit();
-    string get_samples_type();
-    uint32_t get_bit_per_second();
-    double get_length();
+private:
+  /* The "RIFF" chunk descriptor */
+  uint32_t chunk_ID;            // RIFF Header Magic header
+  uint32_t chunk_size;          // RIFF Chunk Size
+  uint32_t format;              // "WAVE" string
+  /* The "fmt" sub-chunk */
+  uint32_t subchunk1_ID;        // "fmt " string
+  uint32_t subchunk1_size;      // Size of the fmt chunk
+  uint16_t audio_format;        // Audio format 1=PCM, 6=mulaw, 7=alaw, 257=IBM Mu-Law, 258=IBM A-Law, 259=ADPCM
+  uint16_t num_of_channels;     // Number of channels 1=Mono 2=Sterio
+  uint32_t samples_per_sec;     // Sampling Frequency in Hz
+  uint32_t bytes_per_sec;       // Bytes per second
+  uint16_t block_align;         // 2=16-bit mono, 4=16-bit stereo
+  uint16_t bits_per_sample;     // Number of bits per sample
+  uint16_t extension_size = 0;  // Size of extension for non-PCM formats
+  /* The "fact" sub-chunk */
+  uint32_t fact_ID = 0;         // "fact" string
+  uint32_t fact_size = 0;       // Size of the fact chunk
+  uint32_t sample_length = 0;   // Byte length of a sample
+  /* The "data" sub-chunk */
+  uint32_t subchunk2_ID;        // "data" string
+  uint32_t subchunk2_size;      // Sampled data length
+  
+public:
+  WavHeader(const std::vector<uint8_t> &byte_file);
+  WavHeader(const std::string &filepath);
+  bool check_validity();
+  uint16_t get_num_of_channels();
+  uint32_t get_frequency();
+  uint16_t get_depth_bit();
+  std::string get_samples_type();
+  uint32_t get_bit_per_second();
+  double get_length();
 };
 
 #endif
