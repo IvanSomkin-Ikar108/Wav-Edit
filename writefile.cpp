d739f5e39aa9a0ec7fa058586f65066792a69a22
Ivan Somkin <somkin.van@yandex.ru>
2022-07-06 04:11:29 +0300

    format writefile.cpp
    Remove unnecessary whitespace.

--- writefile.cpp
+++ writefile.cpp
@@ -2,7 +2,7 @@
 #include <iterator>
 #include "writefile.h"
 
-void writefile (std::vector<uint8_t>& bytes ,std::string filename)
+void writefile(std::vector<uint8_t>& bytes ,std::string filename)
 {
   std::ofstream file (filename, std::ios::out | std::ios::binary | std::ios::trunc);
   std::ostream_iterator<uint8_t> out_itr(file);
